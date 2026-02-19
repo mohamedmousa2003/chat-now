28c9f65421c735773fd5529c468cee37475206b4
Mohamed Mousa <mousa1152003@gmail.com>
2026-02-19 13:15:49 +0200

    finish splash Screen

--- lib/features/splash/splash_view.dart
+++ lib/features/splash/splash_view.dart
@@ -1,10 +1,25 @@
+import 'package:chat_now/config/route_manager/routes.dart';
 import 'package:chat_now/core/utils/app_color.dart';
 import 'package:chat_now/core/utils/images.dart';
 import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) {
+        Navigator.pushReplacementNamed(context, Routes.loginRoute);
+      }
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'config/route_manager/route_generator.dart';
 import 'config/route_manager/routes.dart';
-import 'features/login/presentation/pages/login_view.dart';
-import 'features/register/presentation/pages/register_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,10 +12,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      initialRoute: Routes.splashRoute,
-      onGenerateRoute: RouteGenerator.getRoute,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder: (_, child) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          initialRoute: Routes.splashRoute,
+          onGenerateRoute: RouteGenerator.getRoute,
+        );
+      },
     );
   }
 }
